dfd8369741db72ef8abb7b3fa6e4a56746b87633
nkrabben <n@nkrabben.com>
2017-11-26 17:14:18 -0500

    add test for levels of mcp validation

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 import unittest
-import lxml
+from lxml import etree
 import pymysql
 import os
 import tempfile
 import shutil
+import uuid
 import mcp
+
+
+MCP_LEVEL1 = 'processingMCP'
+MCP_LEVEL2 = 'preconfiguredChoices'
+MCP_LEVEL3 = 'preconfiguredChoice'
+MCP_LEVEL4A = 'appliesTo'
+MCP_LEVEL4B = 'goToChain'
 
 
 class SelfCleaningTestCase(unittest.TestCase):
@@ -32,38 +40,116 @@ class SelfCleaningTestCase(unittest.TestCase):
 class TranslateMCP(SelfCleaningTestCase):
 
   def test_no_file(self):
-    not_file = os.path.join(self.tmpdir, 'a.notfile')
+    not_file_path = os.path.join(self.tmpdir, 'a.notfile')
     with self.assertRaises(OSError) as error_catcher:
-      mcp.MCP(path = not_file)
+      mcp.MCP(path = not_file_path)
 
-    self.assertEqual('No such file or directory: {}'.format(not_file),
+    self.assertEqual('No such file or directory: {}'.format(not_file_path),
       str(error_catcher.exception))
 
 
   def test_not_xml(self):
     not_xml_filename = 'a.notxml'
-    not_xml = os.path.join(self.tmpdir, not_xml_filename)
-    with open(not_xml, 'w+') as f:
+    not_xml_path = os.path.join(self.tmpdir, not_xml_filename)
+    with open(not_xml_path, 'w+') as f:
       f.write('"i_am_json": "lol"')
 
     with self.assertRaises(mcp.MCPError) as error_catcher:
-      mcp.MCP(path = not_xml)
+      mcp.MCP(path = not_xml_path)
 
     self.assertEqual('{} does not contain valid XML'.format(not_xml_filename),
       str(error_catcher.exception))
 
 
-  def test_not_mcp(self):
-    not_mcp_filename = 'a.notcmp'
-    not_mcp = os.path.join(self.tmpdir, not_mcp_filename)
-    with open(not_mcp, 'w+') as f:
-      f.write('<fakeMCP></fakeMCP>')
+  def test_not_mcp_first_level(self):
+    not_mcp_filename = 'a.notmcp'
+    not_mcp_path = os.path.join(self.tmpdir, not_mcp_filename)
+
+    not_mcp_level1 = etree.Element(MCP_LEVEL1[:-1])
+    not_mcp_tree = etree.ElementTree(not_mcp_level1)
+    not_mcp_tree.write(not_mcp_path)
 
     with self.assertRaises(mcp.MCPError) as error_catcher:
-      mcp.MCP(path = not_mcp)
+      mcp.MCP(path = not_mcp_path)
 
     self.assertEqual('{} does not appear to be a valid Archivematica MCP'.format(not_mcp_filename),
       str(error_catcher.exception))
+
+
+  def test_not_mcp_second_level(self):
+    not_mcp_filename = 'a.notmcp'
+    not_mcp_path = os.path.join(self.tmpdir, not_mcp_filename)
+    
+    mcp_level1 = etree.Element(MCP_LEVEL1)
+    not_mcp_level2 = etree.SubElement(not_mcp_level1, MCP_LEVEL2[:-1])
+    not_mcp_tree = etree.ElementTree(not_mcp_level1)
+    not_mcp_tree.write(not_mcp_path)
+
+    with self.assertRaises(mcp.MCPError) as error_catcher:
+      mcp.MCP(path = not_mcp_path)
+
+    self.assertEqual('{} does not appear to be a valid Archivematica MCP'.format(not_mcp_filename),
+      str(error_catcher.exception))
+
+
+  def test_not_mcp_third_level(self):
+    not_mcp_filename = 'a.notmcp'
+    not_mcp_path = os.path.join(self.tmpdir, not_mcp_filename)
+    
+    mcp_level1 = etree.Element(MCP_LEVEL1)
+    mcp_level2 = etree.SubElement(not_mcp_level1, MCP_LEVEL2)
+    for i in range(1,4):
+      etree.SubElement(not_mcp_level2, MCP_LEVEL3[:-1])
+    not_mcp_tree = etree.ElementTree(not_mcp_level1)
+    not_mcp_tree.write(not_mcp_path)
+
+    with self.assertRaises(mcp.MCPError) as error_catcher:
+      mcp.MCP(path = not_mcp_path)
+
+    self.assertEqual('{} does not appear to be a valid Archivematica MCP'.format(not_mcp_filename),
+      str(error_catcher.exception))
+
+
+  def test_not_mcp_fourth_level(self):
+    not_mcp_filename = 'a.notmcp'
+    not_mcp_path = os.path.join(self.tmpdir, not_mcp_filename)
+    
+    mcp_level1 = etree.Element(MCP_LEVEL1)
+    mcp_level2 = etree.SubElement(not_mcp_level1, MCP_LEVEL2)
+    for i in range(1,4):
+      mcp_level3 = etree.SubElement(not_mcp_level2, MCP_LEVEL3)
+      etree.SubElement(not_mcp_level3, MCP_LEVEL4A[:-1])
+      etree.SubElement(not_mcp_level3, MCP_LEVEL4B[:-1])
+    not_mcp_tree = etree.ElementTree(not_mcp_level1)
+    not_mcp_tree.write(not_mcp_path)
+
+    with self.assertRaises(mcp.MCPError) as error_catcher:
+      mcp.MCP(path = not_mcp_path)
+
+    self.assertEqual('{} does not appear to be a valid Archivematica MCP'.format(not_mcp_filename),
+      str(error_catcher.exception))
+
+
+  def test_valid_mcp(self):
+    mcp_filename = 'a.notmcp'
+    mcp_path = os.path.join(self.tmpdir, not_mcp_filename)
+    
+    mcp_level1 = etree.Element(MCP_LEVEL1)
+    mcp_level2 = etree.SubElement(not_mcp_level1, MCP_LEVEL2)
+    for i in range(1,4):
+      mcp_level3 = etree.SubElement(not_mcp_level2, MCP_LEVEL3)
+      mcp_level4a = etree.SubElement(not_mcp_level3, MCP_LEVEL4A)
+      mcp_level4a.text = str(uuid.uuid4())
+      mcp_level4b = etree.SubElement(not_mcp_level3, MCP_LEVEL4B)
+      mcp_level4b.text = str(uuid.uuid4())
+    mcp_tree = etree.ElementTree(not_mcp_level1)
+    mcp_tree.write(not_mcp_path)
+
+    valid_mcp = mcp.MCP(path = not_mcp_path)
+
+    self.assertEqual(valid_mcp.filename, mcp_filename)
+    self.assertEqual(valid_mcp.path, mcp_path)
+    self.assertTrue(valid_mcp.validate())
 
 
 if __name__ == '__main__':
