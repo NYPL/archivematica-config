2e3ca212525b779300e6c0097d74290b1067d6fa
nkrabben <n@nkrabben.com>
2017-11-28 13:08:11 -0500

    test if updated xml is still archivematica valid

--- test.py
+++ test.py
@@ -184,6 +184,23 @@ class TranslateMCP(SelfCleaningTestCase):
     self.assertTrue(all_labels(valid_mcp.tree), ['test_label'])
 
 
+  def test_translated_xml_still_valid(self):
+    mcp_filename = 'a.mcp'
+    mcp_path = os.path.join(self.tmpdir, mcp_filename)
+    
+    mcp_tree = generate_fake_mcp(MCP_LEVEL1, MCP_LEVEL2, MCP_LEVEL3,
+      MCP_LEVEL4A, MCP_LEVEL4B)
+    mcp_tree.write(mcp_path)
+
+    valid_mcp = mcp.MCP(path = mcp_path)
+    uuids = valid_mcp.get_uuids()
+
+    replacement_dict = {uuids[0]['goToChain']: 'test_label'}
+    valid_mcp.set_uuid_labels(replacement_dict)
+
+    self.assertTrue(valid_mcp.validate_mcp())
+
+
   def test_write_xml(self):
     mcp_filename = 'a.mcp'
     mcp_path = os.path.join(self.tmpdir, mcp_filename)
